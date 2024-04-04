aa87e0ec5fbdbb55b6a5329b3d0a14facf738e0c
Victor Garcia Mestre <vgmestre@gmail.com>
2024-04-05 00:09:10 +0200

    exercice 1 and 2

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+import GuessInput from './GuessInput';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +10,19 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guesses, setGuesses] = React.useState([]);
+  return (
+    <>
+      <div className="guess-results">
+        {guesses.map((guess, index) => (
+          <p key={index} className="guess">
+            {guess}
+          </p>
+        ))}
+      </div>
+      <GuessInput onSubmit={(guess) => setGuesses([...guesses, guess])} />
+    </>
+  );
 }
 
 export default Game;
---
+++ src/components/Game/GuessInput.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+const GuessInput = ({ onSubmit }) => {
+  const [guess, setGuess] = useState('');
+
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(e) => {
+        e.preventDefault();
+
+        if (guess.length !== 5) {
+          window.alert('Enter a 5 character word!');
+          return;
+        }
+        onSubmit(guess);
+        setGuess('');
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        required
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        value={guess}
+        onChange={(e) => setGuess(e.target.value.toUpperCase())}
+      />
+    </form>
+  );
+};
+
+export default GuessInput;
